00eab945902da055373c0ed74cabcc2ed4be0cd0
Sabin Dhungana <141560510+SABINXD@users.noreply.github.com>
2024-11-30 17:26:17 +0545

    Update send_code.php

--- assets/php/send_code.php
+++ assets/php/send_code.php
@@ -14,10 +14,10 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'mail.sabcraft.tech';                     //Set the SMTP server to send through
+    $mail->Host       = 'Your Server Host';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'connectify@sabcraft.tech';                     //SMTP username
-    $mail->Password   = 'Savin12#@$';                               //SMTP password
+    $mail->Username   = 'Your Username mail';                     //SMTP username
+    $mail->Password   = 'Your username password';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
